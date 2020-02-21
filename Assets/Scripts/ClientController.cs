910d6185a53eb23b079076dac7335dd5af8bbaf1
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-02-21 14:35:19 -0300

    :fire: Delete ClientUI dependencies

--- Assets/Scripts/ClientController.cs
+++ Assets/Scripts/ClientController.cs
@@ -7,37 +7,28 @@ using System.Text;
 
 public class ClientController : MonoBehaviour
 {
-    struct PendingPing{
-        public int id;
-        public float time;
-    }
-
     private UdpNetworkDriver m_ClientDriver;
     private NativeArray<NetworkConnection> m_clientToServerConnection;
-    // pendingPings is an array of pings sent to the server which have not yet received a response.
-    // Currently we only support one ping in-flight
-    private NativeArray<PendingPing> m_pendingPings;
-    // The ping stats are two integers, time for last ping and number of pings
-    private NativeArray<int> m_pingStats;
 
     private JobHandle m_updateHandle;
+
+    NetworkEndPoint endpoint;
 
     void Start(){
         // Create a NetworkDriver for the client. We could bind to a specific address but in this case we rely on the
         // implicit bind since we do not need to bing to anything special
         m_ClientDriver = new UdpNetworkDriver(new INetworkParameter[0]);
 
-        m_pendingPings = new NativeArray<PendingPing>(64, Allocator.Persistent);
-        m_pingStats = new NativeArray<int>(2, Allocator.Persistent);
         m_clientToServerConnection = new NativeArray<NetworkConnection>(1, Allocator.Persistent);
+
+        endpoint = NetworkEndPoint.LoopbackIpv4;
+        endpoint.Port = 9000;
     }
 
     void OnDestroy(){
         // All jobs must be completed before we can dispose the data they use
         m_updateHandle.Complete();
         m_ClientDriver.Dispose();
-        m_pendingPings.Dispose();
-        m_pingStats.Dispose();
         m_clientToServerConnection.Dispose();
     }
 
@@ -46,8 +37,6 @@ public class ClientController : MonoBehaviour
         public UdpNetworkDriver driver;
         public NativeArray<NetworkConnection> connection;
         public NetworkEndPoint serverEP;
-        public NativeArray<PendingPing> pendingPings;
-        public NativeArray<int> pingStats;
         public float fixedTime;
 
         public void Execute()
@@ -69,11 +58,7 @@ public class ClientController : MonoBehaviour
             {
                 if (cmd == NetworkEvent.Type.Connect)
                 {
-                    // When we get the connect message we can start sending data to the server
-                    // Set the ping id to a sequence number for the new ping we are about to send
-                    pendingPings[0] = new PendingPing {id = pingStats[0], time = fixedTime};
-                    // Create a 4 byte data stream which we can store our ping sequence number in
-                    
+                   
                     /////////////////////////////////////////////////////////////////////////
                     ////////////////////////// SEND DATA TO SERVER /////////////////////
                     DataStreamWriter pingData = ClientData.DirectlyPackCliendData(1, 2,2, 4,4);
@@ -81,8 +66,6 @@ public class ClientController : MonoBehaviour
                     /////////////////////////////////////////////////////////////////////////
 
                     connection[0].Send(driver, pingData);
-                    // Update the number of sent pings
-                    pingStats[0] = pingStats[0] + 1;
                 }
                 else if (cmd == NetworkEvent.Type.Data)
                 {
@@ -95,7 +78,6 @@ public class ClientController : MonoBehaviour
                     /////////////////////////////////////////////////////////////////////////
 
                     // When the pong message is received we calculate the ping time and disconnect
-                    pingStats[1] = (int) ((fixedTime - pendingPings[0].time) * 1000);
                     connection[0].Disconnect(driver);
                     connection[0] = default(NetworkConnection);
                 }
@@ -121,14 +103,11 @@ public class ClientController : MonoBehaviour
 
         // Update the ping client UI with the ping statistics computed by teh job scheduled previous frame since that
         // is now guaranteed to have completed
-        ClientUIController.UpdateStats(m_pingStats[0], m_pingStats[1]);
         PingJob pingJob = new PingJob
         {
             driver = m_ClientDriver,
             connection = m_clientToServerConnection,
-            serverEP = ClientUIController.ServerEndPoint,
-            pendingPings = m_pendingPings,
-            pingStats = m_pingStats,
+            serverEP = endpoint,
             fixedTime = Time.fixedTime
         };
         // Schedule a chain with the driver update followed by the ping job
