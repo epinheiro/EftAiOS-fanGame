86df8f376794e5fc2cec7325492fbb0a5464149a
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-02-21 17:15:27 -0300

    :sparkles::construction: Initial board manipulation

--- Assets/Scripts/Game/BoardManager.cs
+++ Assets/Scripts/Game/BoardManager.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class BoardManager : MonoBehaviour
 {
     public enum PossibleTypes {EventTile, SilentTile, AlienNest, HumanDorm, EscapePod, Empty}
+    public Color[] colors = {Color.gray, Color.white, Color.red, Color.blue, Color.green, Color.black};
+
+    public readonly Vector2 oddRowSpacing = new Vector2(1.9f, 3.3f);
+    public readonly Vector2 evenRowSpacing = new Vector2(0.95f, 1.65f);
+
 
     // Start is called before the first frame update
     void Start()
