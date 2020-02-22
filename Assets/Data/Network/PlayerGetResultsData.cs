33d9ec46324c2b05b6f5b26deb33f71efcb6d5a6
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-02-22 11:39:14 -0300

    :sparkles: First version of GetResults and GetState data classes

---
+++ Assets/Data/Network/PlayerGetResultsData.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Networking.Transport;
+using Unity.Collections;
+
+public class PlayerGetResultsData
+{
+    static int CLASS_HARDCODED_BYTE_SIZE = 8;
+
+
+    static public readonly int commandCode = (int) ServerController.ServerCommand.GetState;
+    public readonly int playerId;
+
+    public PlayerGetResultsData(int playerId){
+        this.playerId = playerId;
+    }
+
+    public PlayerGetResultsData(DataStreamReader reader){
+        DataStreamReader.Context readerCtx = default(DataStreamReader.Context);
+
+        int commandCheck = reader.ReadInt(ref readerCtx);
+
+        if (commandCheck == commandCode){
+            this.playerId = reader.ReadInt(ref readerCtx);
+        }else{
+            readerCtx = default(DataStreamReader.Context);
+            throw new System.Exception(string.Format("Command {0} received is not compatible with command {1}", commandCheck, commandCode));
+        }
+    }
+
+    static public DataStreamWriter CreateAndPackPlayerGetResultsData(int playerId){
+        DataStreamWriter writer = new DataStreamWriter(CLASS_HARDCODED_BYTE_SIZE, Allocator.Temp);
+
+        writer.Write(commandCode);
+        writer.Write(playerId);
+
+        return writer;
+    }
+
+    public DataStreamWriter PackPlayerTurnObjectData(){
+        return PlayerGetResultsData.CreateAndPackPlayerGetResultsData(playerId);
+    }
+
+    public override string ToString(){
+        return string.Format("Command {0} : Player{1})",
+        (ServerController.ServerCommand) commandCode, playerId);
+    }
+}
---
+++ Assets/Data/Network/PlayerGetStateData.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Networking.Transport;
+using Unity.Collections;
+
+public class PlayerGetStateData : MonoBehaviour
+{
+    static int CLASS_HARDCODED_BYTE_SIZE = 12;
+
+
+    static public readonly int commandCode = (int) ServerController.ServerCommand.GetResults;
+    public readonly int playerId;
+
+    public int playerAlive;
+
+    public PlayerGetStateData(int playerId, int isAlive){
+        this.playerId = playerId;
+        this.playerAlive = isAlive;
+    }
+
+    public PlayerGetStateData(DataStreamReader reader){
+        DataStreamReader.Context readerCtx = default(DataStreamReader.Context);
+
+        int commandCheck = reader.ReadInt(ref readerCtx);
+
+        if (commandCheck == commandCode){
+            this.playerId = reader.ReadInt(ref readerCtx);
+            this.playerAlive = reader.ReadInt(ref readerCtx);
+        }else{
+            readerCtx = default(DataStreamReader.Context);
+            throw new System.Exception(string.Format("Command {0} received is not compatible with command {1}", commandCheck, commandCode));
+        }
+    }
+
+    static public DataStreamWriter CreateAndPackPlayerGetStateData(int playerId, int isAlive){
+        DataStreamWriter writer = new DataStreamWriter(CLASS_HARDCODED_BYTE_SIZE, Allocator.Temp);
+
+        writer.Write(commandCode);
+        writer.Write(playerId);
+        writer.Write(isAlive);
+
+        return writer;
+    }
+
+    public DataStreamWriter PackPlayerTurnObjectData(){
+        return PlayerGetStateData.CreateAndPackPlayerGetStateData(playerId, playerAlive);
+    }
+
+    public override string ToString(){
+        return string.Format("Command {0} : Player{1} - Alive{2})",
+        (ServerController.ServerCommand) commandCode, playerId, playerAlive);
+    }
+}
