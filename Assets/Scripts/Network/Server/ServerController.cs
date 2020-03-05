88f062587f086ff60bb9674a940f462bceb9f0b2
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-03-05 09:59:21 -0300

    :sparkles: Show local network IP in Server screen

--- Assets/Scripts/Network/Server/ServerController.cs
+++ Assets/Scripts/Network/Server/ServerController.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using Unity.Networking.Transport;
+using System.Text.RegularExpressions;
+
 
 public class ServerController : MonoBehaviour
 {
     public enum ServerState {SetUp, WaitingPlayers, Processing, Updating}
 
-    ServerState currentState = ServerState.Updating;
+    ServerState currentState = ServerState.SetUp;
 
     ServerCommunication serverCommunication;
+    string serverIp;
 
 
     void Start(){
         serverCommunication = gameObject.AddComponent(typeof(ServerCommunication)) as ServerCommunication;
+        serverIp = GetLocalIPAddress();
+    }
+
+    void OnGUI(){
+        if (currentState == ServerState.SetUp){
+            GUILayout.TextArea(string.Format("Connect to IP: {0}", serverIp));
+        }
     }
 
     // Update is called once per frame
@@ -29,5 +42,26 @@ public class ServerController : MonoBehaviour
             break;
             
         }
+    }
+
+    // Based on the Stackoverflow answer https://stackoverflow.com/a/6803109
+    public static string GetLocalIPAddress()
+    {
+        // https://docs.microsoft.com/pt-br/dotnet/api/system.text.regularexpressions.regex?view=netframework-4.8
+        string pattern = @"^192.168.0.\d*";
+        Regex rgx = new Regex(pattern);
+
+        IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (IPAddress ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                string testedIp = ip.ToString();
+                if(rgx.IsMatch(testedIp)){
+                    return ip.ToString(); 
+                }                
+            }
+        }
+        throw new System.Exception("No network adapters with an IPv4 address in the system!");
     }
 }
