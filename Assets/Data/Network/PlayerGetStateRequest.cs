40c30a329cecca9b6eb4bbe79fe5a01af5a4b3ab
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-03-05 10:26:03 -0300

    :sparkles: Add GetState request and response classes

---
+++ Assets/Data/Network/PlayerGetStateRequest.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Networking.Transport;
+using Unity.Collections;
+
+public static class PlayerGetStateRequest
+{
+    static int CLASS_HARDCODED_BYTE_SIZE = 4;
+
+    static public readonly int commandCode = (int) ServerCommunication.ServerCommand.GetState;
+
+    static public DataStreamWriter PackRequest(){
+        DataStreamWriter writer = new DataStreamWriter(CLASS_HARDCODED_BYTE_SIZE, Allocator.Temp);
+
+        writer.Write(commandCode);
+
+        return writer;
+    }
+}
