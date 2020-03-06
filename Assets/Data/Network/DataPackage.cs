248d653d64284e8c6d779b91982b28af9a5f355d
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-03-06 15:41:51 -0300

    :sparkles: New wrappers to send data with jobs

---
+++ Assets/Data/Network/DataPackage.cs
@@ -0,0 +1,23 @@
+﻿using Unity.Burst;
+using UnityEngine;
+using Unity.Networking.Transport;
+using Unity.Collections;
+using Unity.Jobs;
+using System.Collections.Generic;
+using System;
+
+public static class DataPackageWrapper
+{
+    public static IJob CreateSendDataJob(UdpNetworkDriver driver, NetworkConnection connection, params int[] integersToPack){       
+        NativeArray<int> array = new NativeArray<int>(integersToPack, Allocator.TempJob);
+        
+        SendDataJob sendData = new SendDataJob{
+            driver = driver,
+            connection = connection,
+            varargs = array
+        };
+
+        return sendData;
+    }
+
+}
---
+++ Assets/Data/Network/SendDataJob.cs
@@ -0,0 +1,26 @@
+﻿using Unity.Burst;
+using Unity.Networking.Transport;
+using Unity.Collections;
+using Unity.Jobs;
+
+[BurstCompile]
+struct SendDataJob : IJob {
+    public UdpNetworkDriver driver;
+    public NetworkConnection connection;
+
+    [DeallocateOnJobCompletion]
+    public NativeArray<int> varargs;
+
+    public void Execute(){
+        const int integerByteSize = 4;
+        int dataPackSize = varargs.Length * integerByteSize;
+
+        DataStreamWriter dataPack = new DataStreamWriter(dataPackSize, Allocator.Temp);
+
+        foreach(int arg in varargs){
+            dataPack.Write(arg);
+        }
+
+        connection.Send(driver, dataPack);
+    }
+}
