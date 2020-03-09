097405760942a9681e7de9fcbc53f5d167a145b7
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-03-09 13:56:44 -0300

    :truck::recycle: Make ConnectionUpdateJob a file alone

--- Assets/Scripts/Network/Client/ClientCommunication.cs
+++ Assets/Scripts/Network/Client/ClientCommunication.cs
@@ -1,10 +1,7 @@
-using Unity.Burst;
 using UnityEngine;
 using Unity.Networking.Transport;
 using Unity.Collections;
 using Unity.Jobs;
-using System.Collections.Generic;
-using System;
 
 public class ClientCommunication : MonoBehaviour
 {
@@ -103,28 +100,5 @@ public class ClientCommunication : MonoBehaviour
 
     public void SetClientIdentity(){
         clientId = this.GetComponent<ClientController>().ClientId;
-    }
-}
-
-[BurstCompile]
-struct ConnectionUpdateJob : IJob{
-    public UdpNetworkDriver driver;
-    public NativeArray<NetworkConnection> connection;
-    public NetworkEndPoint serverEP;
-
-    public void Execute()
-    {
-        // If the client ui indicates we should be sending pings but we do not have an active connection we create one
-        if (serverEP.IsValid && !connection[0].IsCreated){
-            Debug.Log("Client reconnection");
-            connection[0] = driver.Connect(serverEP);
-        }
-            
-        // If the client ui indicates we should not be sending pings but we do have a connection we close that connection
-        if (!serverEP.IsValid && connection[0].IsCreated)
-        {
-            connection[0].Disconnect(driver);
-            connection[0] = default(NetworkConnection);
-        }
     }
 }
---
+++ Assets/Scripts/Network/Client/ConnectionUpdateJob.cs
@@ -0,0 +1,28 @@
+﻿using Unity.Burst;
+using UnityEngine;
+using Unity.Networking.Transport;
+using Unity.Collections;
+using Unity.Jobs;
+
+[BurstCompile]
+struct ConnectionUpdateJob : IJob{
+    public UdpNetworkDriver driver;
+    public NativeArray<NetworkConnection> connection;
+    public NetworkEndPoint serverEP;
+
+    public void Execute()
+    {
+        // If the client ui indicates we should be sending pings but we do not have an active connection we create one
+        if (serverEP.IsValid && !connection[0].IsCreated){
+            Debug.Log("Client reconnection");
+            connection[0] = driver.Connect(serverEP);
+        }
+            
+        // If the client ui indicates we should not be sending pings but we do have a connection we close that connection
+        if (!serverEP.IsValid && connection[0].IsCreated)
+        {
+            connection[0].Disconnect(driver);
+            connection[0] = default(NetworkConnection);
+        }
+    }
+}
