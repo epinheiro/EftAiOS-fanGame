a180d97c8ba4b96d39b11bf40ab4c6055b2c7a9f
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-03-09 11:39:08 -0300

    :fire::sparkles: Change legacy GetState data handler to a new one

---
+++ Assets/Scripts/Network/Data/GetStateResponse.cs
@@ -0,0 +1,39 @@
+﻿using Unity.Networking.Transport;
+
+public class GetStateResponse : INetworkData
+{
+    static public readonly int commandCode = (int) ServerCommunication.ServerCommand.GetState;
+    int _serverState;
+    public ServerController.ServerState ServerState{
+        get { return (ServerController.ServerState) _serverState; }
+        set { _serverState = (int) value; }
+    }
+
+    public int[] DataToArray(){
+        return new int[]{commandCode, _serverState};
+    }
+
+    /// <summary>
+    /// This constructor is for WRAPPING the data to make a request
+    /// </summary> 
+    public GetStateResponse(ServerController.ServerState serverState){
+        this._serverState = (int) serverState;
+    }
+
+    /// <summary>
+    /// This constructor is for UNWRAPPING the data to read a request
+    /// </summary> 
+    public GetStateResponse(DataStreamReader reader){
+        DataStreamReader.Context readerCtx = default(DataStreamReader.Context);
+
+        int commandCheck = reader.ReadInt(ref readerCtx);
+
+        if (commandCheck == commandCode){
+            this._serverState = reader.ReadInt(ref readerCtx);
+        }else{
+            readerCtx = default(DataStreamReader.Context);
+            throw new System.Exception(string.Format("Command {0} received is not compatible with this class command {1}", commandCheck, commandCode));
+        }
+    }
+}
+
