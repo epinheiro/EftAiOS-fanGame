8c18e4e3b110698d310ef76fb8795c28560cd99b
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-03-10 15:23:58 -0300

    :recycle: Fix GetResults field name outside of code standard

--- Assets/Scripts/Network/Data/GetResultsRequest.cs
+++ Assets/Scripts/Network/Data/GetResultsRequest.cs
@@ -4,17 +4,17 @@ public class GetResultsRequest : INetworkData
 {
     static public readonly int commandCode = (int) ServerCommunication.ServerCommand.GetResults;
 
-    public int userId;
+    public int playerId;
 
     public int[] DataToArray(){
-        return new int[]{commandCode, userId};
+        return new int[]{commandCode, playerId};
     }
 
     /// <summary>
     /// This constructor is for WRAPPING the data to make a request
     /// </summary> 
-    public GetResultsRequest(int userId){
-        this.userId = userId;
+    public GetResultsRequest(int playerId){
+        this.playerId = playerId;
     }
 
     /// <summary>
@@ -26,7 +26,7 @@ public class GetResultsRequest : INetworkData
         int commandCheck = reader.ReadInt(ref readerCtx);
 
         if (commandCheck == commandCode){
-            this.userId = reader.ReadInt(ref readerCtx);
+            this.playerId = reader.ReadInt(ref readerCtx);
         }else{
             readerCtx = default(DataStreamReader.Context);
             throw new System.Exception(string.Format("Command {0} received is not compatible with this class command {1}", commandCheck, commandCode));
