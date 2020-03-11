4fe56f869fe9947913571b061d1a3cad612ed045
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-03-11 14:29:45 -0300

    :sparkles: New data class to aggregate TileData

---
+++ Assets/Scripts/Game/TileData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileData
+{
+    public readonly string tileCode;
+    public readonly Vector2Int tilePosition;
+    public readonly BoardManager.PossibleTypes type;
+    public readonly GameObject gameObjectReference;
+
+    public TileData(GameObject gameObject, BoardManager.PossibleTypes type, string tileCode, Vector2Int tilePosition){
+        this.gameObjectReference = gameObject;
+        this.type = type;
+        this.tileCode = tileCode;
+        this.tilePosition = tilePosition;
+    }
+}
