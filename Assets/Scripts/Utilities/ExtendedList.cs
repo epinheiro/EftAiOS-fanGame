709e4dfb0617f49b8e15c3ccc2bd6cf5f11e5554
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-03-11 10:56:40 -0300

    :sparkles: Add ExtendedList with some utility methods

---
+++ Assets/Scripts/Utilities/ExtendedList.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Security.Cryptography;
+
+public class ExtendedList<T> : List<T>{
+
+    // Adapted from https://stackoverflow.com/a/1262619
+    private System.Random rng = new System.Random();  
+    public void SimpleShuffle()  
+    {  
+        int n = this.Count;  
+        while (n > 1) {  
+            n--;  
+            int k = rng.Next(n + 1);  
+            T value = this[k];  
+            this[k] = this[n];  
+            this[n] = value;  
+        }  
+    }
+
+    // Adapted from https://stackoverflow.com/a/1262619
+    public void Shuffle()
+    {
+        RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
+        int n = this.Count;
+        while (n > 1)
+        {
+            byte[] box = new byte[1];
+            do provider.GetBytes(box);
+            while (!(box[0] < n * (System.Byte.MaxValue / n)));
+            int k = (box[0] % n);
+            n--;
+            T value = this[k];
+            this[k] = this[n];
+            this[n] = value;
+        }
+    }
+    public void AddRedundant(T listEntry, int numberOfTimes){
+        for( int i=0 ; i<numberOfTimes ; i++ ){
+            this.Add(listEntry);
+        }
+    }
+
+}
