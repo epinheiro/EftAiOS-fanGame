95219d051e4b073137b90fc9dc466ca07b27289b
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-03-19 13:00:20 -0300

    :sparkles: EventDeck to each player to draw when in dangerous sectors

---
+++ Assets/Scripts/Game/Client/EventDeck.cs
@@ -0,0 +1,33 @@
+﻿public class EventDeck
+{
+    public enum CardTypes {CurrentSectorSound, AnySectorSound, NoSound}
+    
+    ///// To future advanced game /////
+    // CurrentSectorSound 10 (4 items)
+    // AnySectorSound 10 (4 items)
+    // NoSound 5
+
+    ExtendedList<CardTypes> deck;
+
+    public EventDeck(){
+        deck = CreateDeck();
+    }
+
+    public ExtendedList<CardTypes> CreateDeck(){
+        ExtendedList<CardTypes> deck = new ExtendedList<CardTypes>();
+
+        deck.AddRedundant(CardTypes.CurrentSectorSound, 10);
+        deck.AddRedundant(CardTypes.AnySectorSound, 10);
+        deck.AddRedundant(CardTypes.NoSound, 5);
+
+        deck.Shuffle();
+
+        return deck;
+    }
+
+    public CardTypes DrawCard(){
+        if(deck.Count == 0) deck = CreateDeck();
+
+        return deck.PopValue();
+    }
+}
