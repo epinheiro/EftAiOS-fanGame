89d9f5b5ba675658094249557c3cf524b5e2fe1e
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-03-30 15:57:24 -0300

    :sparkles: Initial main menu screen to set if application running is server or client

---
+++ Assets/Scripts/MainMenuLoader.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuLoader : MonoBehaviour
+{
+    public void LoadScene(string loadScene){
+        SceneManager.LoadScene(loadScene);
+    }
+}
