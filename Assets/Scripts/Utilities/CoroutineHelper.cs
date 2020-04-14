6dc3bcae923495d686ce097dce38c908936dd97a
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-04-14 18:12:45 -0300

    :recycle::construction: Extract coroutine DelayedCall to a proper CoroutineHelper class

---
+++ Assets/Scripts/Utilities/CoroutineHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CoroutineHelper
+{
+    public delegate void BaseAction();
+
+    static public Coroutine DelayedCall(MonoBehaviour owner, BaseAction function, float delay, bool repeat = false){
+        return owner.StartCoroutine(DelayedCallCoroutine(function, delay, repeat));
+    }
+
+    static IEnumerator DelayedCallCoroutine(BaseAction function, float delay, bool repeat){
+        do{
+            yield return new WaitForSeconds(delay);
+            function();
+        }while(repeat);
+    }
+}
