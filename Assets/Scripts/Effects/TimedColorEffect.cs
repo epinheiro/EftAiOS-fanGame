114b866c1777f7148d43a6b612696a3331742494
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-04-22 17:06:06 -0300

    :art::construction: Player colorized sounds tryouts

---
+++ Assets/Scripts/Effects/TimedColorEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedColorEffect : MonoBehaviour
+{
+    [Range(0.1f, 10f)] public float interval = 1;
+    public Material[] materials;
+    int currentMaterial = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(RotateThroughColors());
+    }
+
+    IEnumerator RotateThroughColors(){
+        while(true){
+            ParticleSystemRenderer psr = GetComponent<ParticleSystemRenderer>();
+
+            currentMaterial = (currentMaterial + 1)%materials.Length;
+            psr.material = materials[currentMaterial];
+
+            yield return new WaitForSecondsRealtime(interval);
+
+        }
+    }
+}
