f94be219a203b9653dc97575fd52c83c6e8eb5ad
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-04-26 18:14:42 -0300

    :art::wrench: Circle effect in text in main menu

--- Assets/Scripts/Effects/CircleMovementEffect.cs
+++ Assets/Scripts/Effects/CircleMovementEffect.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 // Adapted from http://answers.unity.com/answers/596698/view.html
 public class CircleMovementEffect : MonoBehaviour
 {
-    [Range(0,360)] public float beginningAngle = 0;
+    [Range(-360,360)] public float beginningAngle = 0;
     [Range(0, 60)] public float timeToCompleteCircle = 5;
     [Range(0, 10)] public float circleRadius = 5;
 
     float speed;
 
+    Vector3 initialPosition;
+
     void Start(){
+        initialPosition = transform.localPosition;
         speed = (2*Mathf.PI)/timeToCompleteCircle;
     }
 
@@ -20,6 +23,6 @@ public class CircleMovementEffect : MonoBehaviour
         beginningAngle += speed*Time.deltaTime; //if you want to switch direction, use -= instead of +=
         float x = Mathf.Cos(beginningAngle)*circleRadius;
         float y = Mathf.Sin(beginningAngle)*circleRadius;
-        transform.localPosition = new Vector3(x, y);
+        transform.localPosition = initialPosition + new Vector3(x, y);
     }
 }
