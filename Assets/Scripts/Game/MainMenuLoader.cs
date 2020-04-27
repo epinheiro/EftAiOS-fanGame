451004866717ec2acd1456c2edcb09fa8db512c0
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-04-27 17:03:25 -0300

    :sparkles: New About popup linked to Main Menu screen

--- Assets/Scripts/Game/MainMenuLoader.cs
+++ Assets/Scripts/Game/MainMenuLoader.cs
@@ -3,7 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuLoader : MonoBehaviour
 {
+    public GameObject aboutPopup;
+    public GameObject mainMenu;
+
     public void LoadScene(string loadScene){
         SceneManager.LoadScene(loadScene);
+    }
+
+    public void AboutPopup(){
+        aboutPopup.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    public void BackToMainMenu(){
+        aboutPopup.SetActive(false);
+        mainMenu.SetActive(true);
     }
 }
