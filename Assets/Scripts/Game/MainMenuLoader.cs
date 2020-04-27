7d50749c9c746526be37db1fd93e3ef67ef9d9a9
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-04-27 19:00:30 -0300

    :art::sparkles::construction: New How To Play popup in Main Screen

--- Assets/Scripts/Game/MainMenuLoader.cs
+++ Assets/Scripts/Game/MainMenuLoader.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenuLoader : MonoBehaviour
@@ -6,17 +7,34 @@ public class MainMenuLoader : MonoBehaviour
     public GameObject aboutPopup;
     public GameObject mainMenu;
 
+    public GameObject howToPlayPopup;
+    public Scrollbar howtoPlayScrollBar;
+
+    void Start(){
+        howtoPlayScrollBar = howToPlayPopup.transform.Find("Intro").Find("Scrollbar Vertical").GetComponent<Scrollbar>();
+    }
+
     public void LoadScene(string loadScene){
         SceneManager.LoadScene(loadScene);
     }
 
     public void AboutPopup(){
         aboutPopup.SetActive(true);
+        howToPlayPopup.SetActive(false);
         mainMenu.SetActive(false);
     }
 
     public void BackToMainMenu(){
         aboutPopup.SetActive(false);
+        howToPlayPopup.SetActive(false);
         mainMenu.SetActive(true);
+    }
+
+    public void HowToPlayPopup(){
+        aboutPopup.SetActive(false);
+        howToPlayPopup.SetActive(true);
+        mainMenu.SetActive(false);
+
+        howtoPlayScrollBar.value = 1;
     }
 }
