cda3ee54e227674a616393fccfa8e6e14e3c9305
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-04-29 12:48:06 -0300

    :art::wrench: Role popup now show on close the client footer buttons

---
+++ Assets/Scripts/Game/UI/ClientButtonWrapper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientButtonWrapper : MonoBehaviour
+{
+    ClientController controller;
+    UIController uiController;
+    public GameObject uiCanvas;
+
+    void Awake(){
+        controller = this.GetComponent<ClientController>();
+    }
+
+    void Start(){
+        uiController = controller.UIController;
+    }
+
+    public void CloseRolePopup(){
+        uiController.HideRolePopup();
+        uiController.SetActiveClientFooterGroup(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
