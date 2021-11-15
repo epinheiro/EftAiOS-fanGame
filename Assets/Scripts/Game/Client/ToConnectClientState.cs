8d9c092ef3bb02c3e52c1b33d1ee4633a0577910
epinheiro <edu.g.pinheiro@gmail.com>
2021-11-15 10:37:19 -0300

    ♻️Extract IP methods to a more suitable lib class

--- Assets/Scripts/Game/Client/ToConnectClientState.cs
+++ Assets/Scripts/Game/Client/ToConnectClientState.cs
@@ -32,7 +32,7 @@ public class ToConnectClientState : IStateController
                     uiController.SetInsertTextButtonAttributes("ENTER SHIP (SAME PC)");
                 }
             }else{
-                if(NodeCommunication.IsIPValid(_insertedString)){
+                if(IPHelper.IsIPValid(_insertedString)){
                     uiController.SetInsertTextButtonVisibility(true);
                     uiController.SetInsertTextButtonAttributes("ENTER SHIP");
                 }else{
